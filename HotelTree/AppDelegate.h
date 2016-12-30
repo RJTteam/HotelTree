da7c65ab88b172c7ed56092964be225d7b9df7f1
Yangbin <yangbin@yangbins-macbook-pro.local>
2016-12-30 14:31:11 -0600

    Initial Commit

---
+++ HotelTree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotelTree
+//
+//  Created by Yangbin on 12/30/16.
+//  Copyright © 2016 com.rjtcompuquest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HotelTree/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelTree
+//
+//  Created by Yangbin on 12/30/16.
+//  Copyright © 2016 com.rjtcompuquest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
