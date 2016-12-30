e2fd105527aec070c866b4bae26dd63ce64ba73f
Yazhong Luo <kleistluo@gmail.com>
2016-12-30 14:07:37 -0600

    Initial Commit

---
+++ HotelTree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotelTree
+//
+//  Created by Lucas Luo on 12/30/16.
+//  Copyright © 2016 RJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
