05fe7f59e6807be00d9ad77a472b7cfd839316d1
Xinyuan Wang <alexwang@FightUp.local>
2016-12-30 15:27:49 -0600

    first commit test

---
+++ HotelTree/SearchManager.h
@@ -0,0 +1,13 @@
+//
+//  SearchManager.h
+//  HotelTree
+//
+//  Created by Xinyuan Wang on 12/30/16.
+//  Copyright © 2016 RJT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchManager : NSObject
+
+@end
