79927425b1579ca7be15fc70486fec35058e2d2d
Yazhong Luo <kleistluo@gmail.com>
2017-01-01 14:52:00 -0600

    finish map kit part

---
+++ HotelTree/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  HotelTree
+//
+//  Created by Lucas Luo on 1/1/17.
+//  Copyright © 2017 RJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
--- HotelTree/ModelManager.m
+++ HotelTree/ModelManager.m
@@ -57,7 +57,7 @@
 }
 
 - (BOOL)createOrderDBIfNeeded{
-    NSString *orderDBQuery = @"create table if not exists order(orderId integer primary key autoincrement, checkInDate varchar(255) not null, checkOutDate varchar(255) not null, roomNumber varchar(10) not null, adultNumber varchar(255) not null, childrenNumber varchar(255) not null, orderStauts varchar(10) not null, userId varchar(255) not null, hotelId varchar(255) not null);";
+    NSString *orderDBQuery = @"create table if not exists orders(orderId integer primary key autoincrement, checkInDate varchar(255) not null, checkOutDate varchar(255) not null, roomNumber varchar(10) not null, adultNumber varchar(255) not null, childrenNumber varchar(255) not null, orderStauts varchar(10) not null, userId varchar(255) not null, hotelId varchar(255) not null);";
     BOOL result = [[SQLiteManager shareInstance] executeQuery:orderDBQuery];
     return result;
     
@@ -70,11 +70,11 @@
     NSString *userQuery = [NSString stringWithFormat:@"insert into user values('%@','%@');", userId, password];
     BOOL result = [[SQLiteManager shareInstance] executeQuery:userQuery];
     if(result){
-    
-    NSString *userInfoQuery = [NSString stringWithFormat:@"insert into userInfo values('%@', '%@','%@','%@','%@', '%@');", userId, userName, firstName, lastName, email, userAddress];
-    [[SQLiteManager shareInstance] executeQuery:userInfoQuery];
         
-    NSLog(@"Insert Finish");
+        NSString *userInfoQuery = [NSString stringWithFormat:@"insert into userInfo values('%@', '%@','%@','%@','%@', '%@');", userId, userName, firstName, lastName, email, userAddress];
+        [[SQLiteManager shareInstance] executeQuery:userInfoQuery];
+        
+        NSLog(@"Insert Finish");
     }
     
 }
@@ -86,7 +86,7 @@
     
     NSString *filePath = [imageStoreManager imageStore:hotelThumb hotelId:hotelId];
     
-    NSString *hotelQuery = [NSString stringWithFormat:@"insert into hotel values('%@','%@','%@','%@','%@','%@','%@','%@','%@');", hotelId, hotelName,hotelLatitude,hotelLongitude,hotelAddress,hotelRating,hotelPrice,filePath,hotelAvailableDate];
+    NSString *hotelQuery = [NSString stringWithFormat:@"insert into hotel values('%@','%@','%@','%@','%@','%@','%@','%@','%@');", hotelId, hotelName,hotelAddress,hotelLatitude,hotelLongitude,hotelRating,hotelPrice,filePath,hotelAvailableDate];
     
     //!!!hotelAvailableDate should returned by WebService!!!
     
@@ -104,7 +104,7 @@
     
     //NSLog(@"%@",self.filePath);
     
-    NSString *hotelQuery = [NSString stringWithFormat:@"insert into hotel values('%@','%@','%@','%@','%@','%@','%@','%@','%@');", hotel.hotelId, hotel.hotelName,hotel.hotelLatitude,hotel.hotelLongitude,hotel.hotelAddress,hotel.hotelRating,hotel.hotelPrice,filePath,hotel.hotelAvailableDate];
+    NSString *hotelQuery = [NSString stringWithFormat:@"insert into hotel values('%@','%@','%@','%@','%@','%@','%@','%@','%@');", hotel.hotelId, hotel.hotelName,hotel.hotelAddress,hotel.hotelLatitude,hotel.hotelLongitude,hotel.hotelRating,hotel.hotelPrice,filePath,hotel.hotelAvailableDate];
     
     //!!!hotelAvailableDate should returned by WebService!!!
     
@@ -125,7 +125,7 @@
 
 -(BOOL)createOrder:(NSDate *)checkInDate checkOutDate:(NSDate *)checkOutDate roomNumber:(NSString *)roomNumber adultNumber:(NSString *)adultNumber childrenNumber:(NSString *)childrenNumber orderStauts:(NSString *)orderStauts userId:(NSString *)userId hotelId:(NSString *)hotelId{
     
-    NSString *orderQuery = [NSString stringWithFormat:@"insert into order values(NULL,'%@','%@','%@','%@','%@','%@','%@','%@');", checkInDate,checkOutDate,roomNumber,adultNumber,childrenNumber,orderStauts,userId,hotelId];
+    NSString *orderQuery = [NSString stringWithFormat:@"insert into orders values(NULL,'%@','%@','%@','%@','%@','%@','%@','%@');", checkInDate,checkOutDate,roomNumber,adultNumber,childrenNumber,orderStauts,userId,hotelId];
     
     BOOL result = [[SQLiteManager shareInstance] executeQuery:orderQuery];
     
@@ -133,14 +133,14 @@
 }
 
 - (NSArray*)getAllUser{
-        NSMutableArray *allUsers = [[NSMutableArray alloc]init];
-        NSString *fetchQuery = [NSString stringWithFormat:@"select * from user join userInfo on user.userId = userInfo.userId;"];
-        NSArray *array = [[SQLiteManager shareInstance] executeQueryWithStatement:fetchQuery];
+    NSMutableArray *allUsers = [[NSMutableArray alloc]init];
+    NSString *fetchQuery = [NSString stringWithFormat:@"select * from user join userInfo on user.userId = userInfo.userId;"];
+    NSArray *array = [[SQLiteManager shareInstance] executeQueryWithStatement:fetchQuery];
     
-        for(NSDictionary *dic in array){
-            User *newUser = [[User alloc] initWithDictionary:dic];
-            [allUsers addObject:newUser];
-        }
+    for(NSDictionary *dic in array){
+        User *newUser = [[User alloc] initWithDictionary:dic];
+        [allUsers addObject:newUser];
+    }
     return allUsers;
 }
 
@@ -158,7 +158,7 @@
 
 -(NSArray*)getAllOrderByUserId:(NSString *)userId{
     NSMutableArray *allOrder = [[NSMutableArray alloc]init];
-    NSString *fetchQuery = [NSString stringWithFormat:@"select * from order where userId = '%@';",userId];
+    NSString *fetchQuery = [NSString stringWithFormat:@"select * from orders where userId = '%@';",userId];
     NSArray *array = [[SQLiteManager shareInstance] executeQueryWithStatement:fetchQuery];
     
     for(NSDictionary *dic in array){
