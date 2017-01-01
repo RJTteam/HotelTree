180c7aad5f626e31a2561570ba246fc95b5ecafb
Yazhong Luo <kleistluo@gmail.com>
2017-01-01 10:19:43 -0600

    AlexNeedChangeSearchManage

---
+++ HotelTree/UserSearchResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserSearchResultViewController.h
+//  SQLiteDemo
+//
+//  Created by Xinyuan Wang on 12/30/16.
+//  Copyright © 2016 RJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserSearchResultViewController : UITableViewController
+
+@end
