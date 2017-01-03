6ce1af0b86a8f6263f6b9806353350c29230ee86
Xinyuan Wang <alexwang@FightUp.local>
2017-01-02 18:36:30 -0600

    add calendar to detail view

--- HotelTree/DetailViewController.m
+++ HotelTree/DetailViewController.m
@@ -10,16 +10,25 @@
 #import <CoreLocation/CoreLocation.h>
 #import <MapKit/MapKit.h>
 #import "Annotation.h"
+#import "PMCalendar.h"
 
 #define METERS_PRE_MILE 1609.34
 
-@interface DetailViewController ()
+@interface DetailViewController ()<PMCalendarControllerDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UIButton *toMapBtn;
 @property (weak, nonatomic) IBOutlet UILabel *priceLabel;
 @property (weak, nonatomic) IBOutlet UILabel *rantingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *checkInWeekdayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *checkOutWeekdayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *checkOutMonthLabel;
+@property (weak, nonatomic) IBOutlet UILabel *checkInMonthLabel;
 
 @property (weak, nonatomic) IBOutlet MKMapView *aHotelMap;
+
+@property (strong, nonatomic)PMCalendarController *calendar;
+@property (strong, nonatomic)NSDictionary *numberToStringDict;
+
 @end
 
 @implementation DetailViewController{
@@ -31,6 +40,14 @@
     self.nameLabel.text = self.aHotel.hotelName;
     self.priceLabel.text = self.aHotel.price;
     self.rantingLabel.text = self.aHotel.hotelRating;
+    
+    self.numberToStringDict = @{[NSNumber numberWithInteger:1]:@"Mon",
+                                [NSNumber numberWithInteger:2]:@"Tue",
+                                [NSNumber numberWithInteger:3]:@"Wed",
+                                [NSNumber numberWithInteger:4]:@"Thu",
+                                [NSNumber numberWithInteger:5]:@"Fri",
+                                [NSNumber numberWithInteger:6]:@"Sat",
+                                [NSNumber numberWithInteger:7]:@"Sun"};
     
     _coordinate.longitude = [[NSString stringWithFormat:@"-%@",self.aHotel.hotelLong] doubleValue];
     
@@ -49,7 +66,35 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark - Event Methods
 
+- (IBAction)openCalendarButtonClicked:(UIButton *)sender {
+    if(self.calendar != nil && self.calendar.isCalendarVisible){
+        [self.calendar dismissCalendarAnimated:YES];
+    }else{
+        self.calendar = [[PMCalendarController alloc] initWithSize:CGSizeMake(300, 170)];
+        self.calendar.delegate = self;
+        [self.calendar setShowOnlyCurrentMonth:YES];
+        [self.calendar presentCalendarFromView:sender permittedArrowDirections:PMCalendarArrowDirectionDown animated:YES];
+    }
+}
+
+#pragma mark - PMCalendarControllerDelegate
+
+- (BOOL)calendarControllerShouldDismissCalendar:(PMCalendarController *)calendarController{
+    PMPeriod *period = calendarController.period;
+    NSDate *start = period.startDate;
+    NSDate *end = period.endDate;
+    NSCalendar *cal = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    cal.timeZone = [NSTimeZone localTimeZone];
+    NSDateComponents *startComponents = [cal components:NSCalendarUnitWeekday|NSCalendarUnitDay | NSCalendarUnitMonth fromDate:start];
+    NSDateComponents *endComponents = [cal components:NSCalendarUnitWeekday|NSCalendarUnitDay | NSCalendarUnitMonth fromDate:end];
+    self.checkInWeekdayLabel.text = [NSString stringWithFormat:@"%@,%lu", self.numberToStringDict[[NSNumber numberWithInteger:startComponents.weekday]], startComponents.day];
+    self.checkInMonthLabel.text = [NSString stringWithFormat:@"%lu", startComponents.month];
+    self.checkOutWeekdayLabel.text = [NSString stringWithFormat:@"%@,%lu", self.numberToStringDict[[NSNumber numberWithInteger:endComponents.weekday]], endComponents.day];
+    self.checkOutMonthLabel.text = [NSString stringWithFormat:@"%lu", endComponents.month];
+    return YES;
+}
 /*
 #pragma mark - Navigation
 
